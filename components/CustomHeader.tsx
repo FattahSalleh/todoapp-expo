cea36df0833e60290c2459ec0b23d9090346b826
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-05-21 09:20:49 +0800

    feat: add SafeAreaView for CustomHeader

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,19 +1,43 @@
-import React, { Requireable } from "react";
-import { View, Text, Image, StyleSheet, ImageSourcePropType } from "react-native";
+import React, { Requireable, useState } from "react";
+import {
+	View,
+	Text,
+	Image,
+	StyleSheet,
+	ImageSourcePropType,
+	useColorScheme,
+} from "react-native";
 import { ThemedView } from "./ThemedView";
 import { ThemedText } from "./ThemedText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "@/constants/Colors";
 
 interface CustomHeaderProps {
 	title: string;
-	iconSource: string | any; 
+	iconSource: string | any;
 }
 
 const CustomHeader: React.FC<CustomHeaderProps> = ({ title, iconSource }) => {
+	const deviceTheme = useColorScheme();
+
+	const [currentTheme, setCurrentTheme] = useState(deviceTheme);
+
+	const toggleTheme = () => {
+		setCurrentTheme(currentTheme === "dark" ? "light" : "dark");
+	};
+
+	const iconThemeSrc =
+		currentTheme === "dark"
+			? require("@/assets/images/light_mode.svg")
+			: require("@/assets/images/dark_mode.svg");
+
 	return (
-		<ThemedView style={styles.headerContainer}>
-			<Image source={iconSource} style={styles.icon} />
-			<ThemedText style={styles.title}>{title}</ThemedText>
-		</ThemedView>
+		<SafeAreaView edges={["top"]} style={{ backgroundColor: Colors.dark.background }}>
+			<ThemedView style={styles.headerContainer}>
+				<Image source={iconSource} style={styles.icon} />
+				<ThemedText style={styles.title}>{title}</ThemedText>
+			</ThemedView>
+		</SafeAreaView>
 	);
 };
 
@@ -22,7 +46,7 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		alignItems: "center",
 		padding: 16,
-		paddingTop: 48
+		// paddingTop: 48
 	},
 	icon: {
 		width: 24,
