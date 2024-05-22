bec13db5cc11f7923d2b5e9555c54e74fd64b248
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-05-22 09:34:40 +0800

    chore: add more data types to display in Todo FlatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,32 +4,25 @@ import { useTheme } from "@/context/ThemeContext";
 import { StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 
-const todoData = [
-	{ id: "1", title: "Task 1" },
-	{ id: "2", title: "Task 2" },
-	{ id: "3", title: "Task 3" },
-	{ id: "4", title: "Task 4" },
-	{ id: "5", title: "Task 5" },
-	{ id: "6", title: "Task 6" },
-	{ id: "7", title: "Task 7" },
-	{ id: "8", title: "Task 8" },
-	{ id: "9", title: "Task 9" },
-	{ id: "10", title: "Task 10" },
-	{ id: "11", title: "Task 11" },
-	{ id: "12", title: "Task 12" },
-	{ id: "13", title: "Task 13" },
-	{ id: "14", title: "Task 14" },
-	{ id: "15", title: "Task 15" },
-	{ id: "16", title: "Task 16" },
-	{ id: "17", title: "Task 17" },
-	{ id: "18", title: "Task 18" },
-	{ id: "19", title: "Task 19" },
-	{ id: "20", title: "Task 20" },
+type TodoData = {
+	id: string;
+	title: string;
+	description: string;
+	date_created: Date;
+};
+
+const todoData: TodoData[] = [
+	{
+		id: "1",
+		title: "Task 1",
+		description: "Description for Task 1",
+		date_created: new Date("2024-05-21"),
+	},
 ];
 
-type ItemProps = { title: string };
+type TodoProps = { todoData: TodoData };
 
-const Item = ({ title }: ItemProps) => {
+const Item = ({ todoData }: TodoProps) => {
 	const { theme } = useTheme();
 
 	const itemBackgroundColor = theme === "dark" ? "#027148" : "#00FBB0";
@@ -37,17 +30,23 @@ const Item = ({ title }: ItemProps) => {
 		<ThemedView
 			style={[styles.item, { backgroundColor: itemBackgroundColor }]}
 		>
-			<ThemedText style={styles.title}>{title}</ThemedText>
+			<ThemedText style={styles.title}>{todoData.title}</ThemedText>
+			<ThemedText style={styles.description}>
+				{todoData.description}
+			</ThemedText>
+			<ThemedText style={styles.date}>
+				Created on {todoData.date_created.toLocaleDateString()}
+			</ThemedText>
 		</ThemedView>
 	);
 };
 
 export default function TodoScreen() {
 	return (
-		<ThemedView>
+		<ThemedView style={styles.container}>
 			<FlatList
 				data={todoData}
-				renderItem={({ item }) => <Item title={item.title} />}
+				renderItem={({ item }) => <Item todoData={item} />}
 				keyExtractor={(item) => item.id}
 			/>
 		</ThemedView>
@@ -55,6 +54,9 @@ export default function TodoScreen() {
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
 	item: {
 		padding: 20,
 		marginVertical: 8,
@@ -63,5 +65,13 @@ const styles = StyleSheet.create({
 	},
 	title: {
 		fontSize: 24,
+		fontWeight: "bold",
+	},
+	description: {
+		fontSize: 16,
+	},
+	date: {
+		fontSize: 12,
+		textAlign: "right",
 	},
 });
