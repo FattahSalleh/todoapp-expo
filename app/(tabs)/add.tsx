8bc14406f749aacb8aa53e17ada3845457369ad8
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-05-24 12:01:04 +0800

    feat: add save data to json file using expo file system

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Keyboard, StyleSheet, View } from "react-native";
+import { Alert, Keyboard, StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { ScrollView } from "react-native-gesture-handler";
@@ -6,17 +6,42 @@ import { useState } from "react";
 import { ThemedTextInput } from "@/components/ThemedTextInput";
 import { ThemedButton } from "@/components/ThemedButton";
 import todoData from "@/db/todo-data.json";
+import * as FileSystem from "expo-file-system";
 
 export default function AddScreen() {
 	const [textTitle, onChangeTextTitle] = useState("");
 	const [textDesc, onChangeTextDesc] = useState("");
+	const [todos, setTodos] = useState(todoData);
 
 	const handleClear = () => {
 		onChangeTextTitle("");
 		onChangeTextDesc("");
 	};
 
-	const handleSubmit = () => {
+	const saveTodoDataToJsonFile = async (
+		data: {
+			id: string;
+			title: string;
+			description: string;
+			date_created: string;
+		}[]
+	) => {
+		const jsonData = JSON.stringify(data);
+		let filePath = FileSystem.documentDirectory + "/todo-data.json";
+
+		try {
+			await FileSystem.writeAsStringAsync(filePath, jsonData, {
+				encoding: FileSystem.EncodingType.UTF8,
+			});
+			Alert.alert("Success!", "Succeeded to save todo data.");
+			console.log("Todo data saved successfully.");
+		} catch (error) {
+			console.error("Error writing todo data: ", error);
+			Alert.alert("Error", "Failed to save todo data. Please try again.");
+		}
+	};
+
+	const handleSubmit = async () => {
 		if (!textTitle.trim() || !textDesc.trim()) {
 			Alert.alert("Error", "Please enter both title and description.");
 			return;
@@ -31,6 +56,10 @@ export default function AddScreen() {
 
 		// Update the todoData array with new Todo item
 		todoData.push(newTodo);
+
+		// Save the updated todoData array to JSON file
+		await saveTodoDataToJsonFile(todoData);
+		setTodos([...todos, newTodo]);
 
 		// Reset input fields
 		onChangeTextTitle("");
