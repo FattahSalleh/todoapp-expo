41e4f139d7e9856f9eb2984a73b61e9b04f2a317
Fattah Katsana <fattah.sallehuddin@gmail.com>
2024-05-24 08:51:37 +0800

    feat: add feature to fetch data from todo-data.json and parse Date

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { ThemedView } from "@/components/ThemedView";
 import { useTheme } from "@/context/ThemeContext";
 import { StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
+import todoData from "@/db/todo-data.json";
 
 type TodoData = {
 	id: string;
@@ -10,99 +11,6 @@ type TodoData = {
 	description: string;
 	date_created: Date;
 };
-
-const todoData: TodoData[] = [
-	{
-		id: "1",
-		title: "Task 1",
-		description: "Description for Task 1",
-		date_created: new Date("2024-05-21"),
-	},
-	{
-		id: "2",
-		title: "Task 2",
-		description: "Description for Task 2",
-		date_created: new Date("2024-05-20"),
-	},
-	{
-		id: "3",
-		title: "Task 3",
-		description: "Description for Task 3",
-		date_created: new Date("2024-05-19"),
-	},
-	{
-		id: "4",
-		title: "Task 4",
-		description: "Description for Task 4",
-		date_created: new Date("2024-05-18"),
-	},
-	{
-		id: "5",
-		title: "Task 5",
-		description: "Description for Task 5",
-		date_created: new Date("2024-05-17"),
-	},
-	{
-		id: "6",
-		title: "Task 6",
-		description: "Description for Task 6",
-		date_created: new Date("2024-05-16"),
-	},
-	{
-		id: "7",
-		title: "Task 7",
-		description: "Description for Task 7",
-		date_created: new Date("2024-05-15"),
-	},
-	{
-		id: "8",
-		title: "Task 8",
-		description: "Description for Task 8",
-		date_created: new Date("2024-05-14"),
-	},
-	{
-		id: "9",
-		title: "Task 9",
-		description: "Description for Task 9",
-		date_created: new Date("2024-05-13"),
-	},
-	{
-		id: "10",
-		title: "Task 10",
-		description: "Description for Task 10",
-		date_created: new Date("2024-05-12"),
-	},
-	{
-		id: "11",
-		title: "Task 11",
-		description: "Description for Task 11",
-		date_created: new Date("2024-05-11"),
-	},
-	{
-		id: "12",
-		title: "Task 12",
-		description: "Description for Task 12",
-		date_created: new Date("2024-05-10"),
-	},
-	{
-		id: "13",
-		title: "Task 13",
-		description: "Description for Task 13",
-		date_created: new Date("2024-05-09"),
-	},
-	{
-		id: "14",
-		title: "Task 14",
-		description: "Description for Task 14",
-		date_created: new Date("2024-05-08"),
-	},
-	{
-		id: "15",
-		title: "Task 15",
-		description: "Description for Task 15",
-		date_created: new Date("2024-05-07"),
-	},
-];
 
 // Change Item to Collapsibles for Description. Sample usage:
 {
@@ -144,10 +52,16 @@ const Item = ({ todoData }: TodoProps) => {
 };
 
 export default function TodoScreen() {
+	// Convert string to Date format
+	const todosConvertDate = todoData.map((todo) => ({
+		...todo,
+		date_created: new Date(todo.date_created),
+	}));
+
 	return (
 		<ThemedView style={styles.container}>
 			<FlatList
-				data={todoData}
+				data={todosConvertDate}
 				renderItem={({ item }) => <Item todoData={item} />}
 				keyExtractor={(item) => item.id}
 			/>
